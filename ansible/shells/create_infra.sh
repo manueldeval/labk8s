5f5e97c06c1b8ffc2157e7a7dff5fa576ffb91dd
Manuel Deval <manuel.deval@gmail.com>
2018-10-13 19:43:39 +0200

    Suppression des dépendances a l'env de dev.

--- ansible/shells/create_infra.sh
+++ ansible/shells/create_infra.sh
@@ -7,12 +7,5 @@ ANSIBLE_DIR=$SCRIPT_DIR/..
 cd $ANSIBLE_DIR
 
 ansible-playbook -i environments/lab_host create_infra.yml --ask-become-pass
-ssh-keygen -f "/home/deman/.ssh/known_hosts" -R "10.0.4.5" &> /dev/null
-ssh-keygen -f "/home/deman/.ssh/known_hosts" -R "10.0.4.6" &> /dev/null
-ssh-keygen -f "/home/deman/.ssh/known_hosts" -R "10.0.4.11" &> /dev/null
-ssh-keygen -f "/home/deman/.ssh/known_hosts" -R "10.0.4.12" &> /dev/null
-ssh-keygen -f "/home/deman/.ssh/known_hosts" -R "10.0.4.14" &> /dev/null
-ssh-keygen -f "/home/deman/.ssh/known_hosts" -R "10.0.4.15" &> /dev/null
-ssh-keygen -f "/home/deman/.ssh/known_hosts" -R "10.0.4.16" &> /dev/null
 
 cd $PWD
