489f66f9af7c913324df97f03ca014d4263e8da1
Manuel Deval <manuel.deval@gmail.com>
2018-10-22 09:43:20 +0200

    Gestion des backup restore de l'infra.

---
+++ ansible/shells/restore_infra.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+PWD="$(pwd)"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ANSIBLE_DIR=$SCRIPT_DIR/..
+
+cd $ANSIBLE_DIR
+
+ansible-playbook -i environments/lab_host restore_infra.yml --ask-become-pass
+
+cd $PWD
